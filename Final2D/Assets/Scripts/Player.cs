ca014d5dbe034059d626466762b1e1e6ec92477a
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2023-05-09 12:34:13 -0400

    Changed physics updates to FixedUpdate()

--- Final2D/Assets/Scripts/Player.cs
+++ Final2D/Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private AudioSource audioThruster;
-    [SerializeField] private AudioClip soundThruster;
+/*    [SerializeField] private AudioClip soundThruster;*/
 
     private float rotSpeed;
     private float thrustAmount;
@@ -18,7 +18,7 @@ public class Player : MonoBehaviour
     {
         playAudio = false;
         rotSpeed = 15;
-        thrustAmount = 7.5f;
+        thrustAmount = 250;
     }
 
     // Update is called once per frame
@@ -27,21 +27,12 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             GetComponent<Transform>().eulerAngles += new Vector3(0, 0, rotSpeed);
-           //transform.Rotate(Vector3.forward * rotSpeed * Time.deltaTime);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             GetComponent<Transform>().eulerAngles += new Vector3(0, 0, -rotSpeed);
-            //transform.Rotate(Vector3.forward * -rotSpeed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.W))
-        {
-            //Debug.Log("Audio On");
-            playAudio = true;
-            rb.AddForce(transform.up * thrustAmount);
-            //transform.position += transform.up * thrustAmount;
-            //transform.Rotate(Vector3.forward * -rotSpeed * Time.deltaTime);
-        }
+
         if (Input.GetKeyUp(KeyCode.W))
         {
             Debug.Log("Audio Off");
@@ -56,9 +47,17 @@ public class Player : MonoBehaviour
             if (!audioThruster.isPlaying)
             {
                 Debug.Log("Audio On");
-                audioThruster.PlayOneShot(soundThruster);
-                //audioThruster.Play();
+                audioThruster.Play();
             }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            playAudio = true;
+            rb.AddForce(transform.up * thrustAmount);
         }
     }
 }
