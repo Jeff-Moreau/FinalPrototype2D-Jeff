00a69cb87d069ae48145cda182810148b303f4fc
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2023-05-09 15:54:06 -0400

    Insert Coins timer done

--- Final2D/Assets/Scripts/GameLoop.cs
+++ Final2D/Assets/Scripts/GameLoop.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class GameLoop : MonoBehaviour
 {
     [SerializeField] private GameObject thePlayer;
+    [SerializeField] private int fuelAmount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<Hud>().fuelTotal.text = "" + 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.C) && fuelAmount <6000)
+        {
+            fuelAmount += 750;
+            gameObject.GetComponent<Hud>().fuelTotal.text = fuelAmount.ToString();
+        }
     }
 }
--- Final2D/Assets/Scripts/Hud.cs
+++ Final2D/Assets/Scripts/Hud.cs
@@ -7,23 +7,40 @@ public class Hud : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreTotal;
     [SerializeField] private TextMeshProUGUI timeTotal;
-    [SerializeField] private TextMeshProUGUI fuelTotal;
+    [SerializeField] public TextMeshProUGUI fuelTotal;
     [SerializeField] private TextMeshProUGUI altitudeCurrent;
     [SerializeField] private TextMeshProUGUI horSpeedCurrent;
     [SerializeField] private TextMeshProUGUI verSpeedCurrent;
     [SerializeField] private TextMeshProUGUI horSpeedArrow;
     [SerializeField] private TextMeshProUGUI verSpeedArrow;
+    [SerializeField] private TextMeshProUGUI insertCoins;
     [SerializeField] private GameObject thePlayer;
 
+    private float timerBlink;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timerBlink = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-  
+        timerBlink += Time.deltaTime;
+
+        Debug.Log(timerBlink);
+        if (timerBlink < 1.5f)
+        {
+            insertCoins.enabled = true;
+        }
+        else if (timerBlink > 1.5f && timerBlink < 2f)
+        {
+            insertCoins.enabled = false;
+        }
+        else
+        {
+            timerBlink = 0;
+        }
+        
     }
 }
